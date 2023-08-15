0abf0d6788e06ccfa694c1cdfbaed8ed7f250599
seancarroll <seanc28@gmail.com>
2023-08-15 08:34:00 -0500

    cleanup

--- src/cli.rs
+++ src/cli.rs
@@ -2,13 +2,6 @@ use clap::{Parser, Subcommand};
 
 use crate::cmd::*;
 
-// use constants that can also be used as defaults
-// https://github.com/TeXitoi/structopt/issues/226
-
-// TODO: add config.rs?
-
-// TODO: environment variable fallback here or via config?
-// should document it here regardless
 #[derive(Parser, Debug)]
 #[command(about, version, author)]
 pub struct Args {
