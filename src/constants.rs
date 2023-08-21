0cc978b4eb6af0339bf17050b4d2889914567aec
seancarroll <seanc28@gmail.com>
2023-08-20 23:07:45 -0500

    default output directory to generated-snippets

--- src/constants.rs
+++ src/constants.rs
@@ -1,12 +1,12 @@
+pub const SNIPPEXT: &'static str = "snippext";
 // TODO: this might not be needed
 pub const DEFAULT_SNIPPEXT_CONFIG: &str = include_str!("./default_snippext_config.yaml");
-pub const DEFAULT_CONFIG: &'static str = "snippext";
 pub const DEFAULT_BEGIN: &'static str = "snippet::start::";
 pub const DEFAULT_END: &'static str = "snippet::end::";
 pub const DEFAULT_INCLUDE: &'static str = "snippet::include::";
 pub const DEFAULT_TEMPLATE: &'static str = "{{snippet}}";
 pub const DEFAULT_OUTPUT_FILE_EXTENSION: &'static str = "md";
 pub const DEFAULT_SOURCE_FILES: &'static str = "**";
-pub const DEFAULT_OUTPUT_DIR: &'static str = "./snippets/";
+pub const DEFAULT_OUTPUT_DIR: &'static str = "./generated-snippets/";
 pub const SNIPPEXT_TEMPLATE_ATTRIBUTE: &'static str = "template";
 pub const DEFAULT_TEMPLATE_IDENTIFIER: &'static str = "default";
