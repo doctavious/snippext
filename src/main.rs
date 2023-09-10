51d2330208c512a228f717bb243680455f00fe1d
seancarroll <seanc28@gmail.com>
2023-09-10 00:05:55 -0500

    remove TODOs

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,6 @@ use snippext::{cli, SnippextResult};
 use tracing::Level;
 use tracing_subscriber;
 
-// TODO priorities
-// 1. refactor to subcommands
-//      . init - generate config with prompts
-//      . extract - extract snippets
-//      . clear - clear snippets from targets
-// 2. updating target files should read lines instead of whole file - done
-// 3. add file name and path attributes - done
-// 4. we should keep snippet name when writing out snippet to target file
-// 5. validate error messages
-// 6. docs
-// 7. publish to crates.io
-// 8. create bin / script to download and install
-
-// static DEFAULT_CONFIG: &'static str = include_str!("default_snippext.yaml");
-
-// split into subcommands?? does extract combine generate and write?
-// 1. generate - output to dir
-// 2. write - write to target files
-// 3. clear - clear up generate or files
-// 4. init - generate config file
-
 fn main() -> SnippextResult<()> {
     let opt = cli::Args::parse();
 
